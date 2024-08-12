73eb1bfdfd1c2d805cc2f98ec8d4d7969d8cb0f7
Jasaman Singh <you@example.com>
2024-08-12 14:08:03 +0530

    deploying my BLog Project

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Django Blog Project</h1>
+This is a Blog project based on Django.
+<br>
+Note: This project is made from the guidence of EngineerCode Summer Internship Program.
---
+++ blog_app/views.py
@@ -0,0 +1,137 @@
+from multiprocessing import context
+from django.shortcuts import render, get_object_or_404, redirect
+from blog_app.forms import PostForm, CommentForm
+from blog_app.models import Post, Comment
+from django.utils import timezone
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+
+# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active: 
+                login(request, user)
+                return HttpResponseRedirect(reverse('blog_app:post_list'))
+            else:
+                return HttpResponse('Account not found!')
+        else:
+            print('Someone tried to login and failed')
+            print('Username: {} and password {}'.format(username, password))
+            return HttpResponse('Invalid login details supplied')
+    else:
+        return render(request, 'registration/login.html', {})
+    
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('blog_app:post_list'))
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+    context_object_name = 'about'
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog_app/post_list.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') # -published_date is to order the posts in descending order
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog_app/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_list.html'
+    form_class = PostForm
+    model = Post
+    template_name = 'blog_app/post_form.html'
+    context_object_name = 'post'
+    # fields = ['author', 'title', 'text']
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_detail.html'
+    form_class = PostForm
+    model = Post
+    template_name = 'blog_app/post_update.html'
+    # fields = ['author', 'title', 'text']
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog_app:post_list')
+    template_name = 'blog_app/post_confirm_delete.html'
+    content_object_name = 'object'
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/draft_list.html'
+    model = Post
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
+    
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+
+    return redirect('blog_app:post_detail', pk=pk)
+
+
+# Comment views
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('blog_app:post_detail', pk=post.pk)
+    
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog_app/comment_form.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+
+    return redirect('blog_app:post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+
+    return redirect('blog_app:post_detail', pk=post_pk)
